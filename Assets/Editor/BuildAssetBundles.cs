def55906bb664bc2a8dbe3fbb7c8816257f7fdbc
treerootCompany <treerootke@gmail.com>
2024-10-08 19:21:53 +0800

    push company's content

--- Assets/Editor/BuildAssetBundles.cs
+++ Assets/Editor/BuildAssetBundles.cs
@@ -6,34 +6,34 @@ public class BuildAssetBundles
     [MenuItem("Assets/Build AssetBundles")]
     public static void BuildAllAssetBundles()
     {
-        //string assetBundleDirectory = "Assets/StreamingAssets";  
+        string assetBundleDirectory = "Assets/StreamingAssets";  
 
-        //if (!Directory.Exists(assetBundleDirectory))  
-        //{  
-        //    Directory.CreateDirectory(assetBundleDirectory);  
-        //}  
+        if (!Directory.Exists(assetBundleDirectory))  
+        {  
+            Directory.CreateDirectory(assetBundleDirectory);  
+        }  
 
-        //// 设置打包选项: LZ4压缩、无加密
-        //BuildAssetBundleOptions assetBundleOptions = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle;  
-        //BuildPipeline.BuildAssetBundles(assetBundleDirectory, assetBundleOptions, BuildTarget.StandaloneWindows64);
-        BuildTarget[] targetPlatforms = new BuildTarget[]
-        {
-            BuildTarget.StandaloneWindows64, 
-            BuildTarget.Android, 
-        };
+        // 设置打包选项: LZ4压缩、无加密
+        BuildAssetBundleOptions assetBundleOptions = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle;  
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, assetBundleOptions, BuildTarget.StandaloneWindows64);
+        // BuildTarget[] targetPlatforms = new BuildTarget[]
+        // {
+            // BuildTarget.StandaloneWindows64, 
+            // BuildTarget.Android, 
+        // };
 
-        foreach (var target in targetPlatforms)
-        {
-            string assetBundleDirectory = "Assets/StreamingAssets_" + target.ToString();
+        // foreach (var target in targetPlatforms)
+        // {
+            // string assetBundleDirectory = "Assets/StreamingAssets_" + target.ToString();
 
-            if (!Directory.Exists(assetBundleDirectory))
-            {
-                Directory.CreateDirectory(assetBundleDirectory);
-            }
+            // if (!Directory.Exists(assetBundleDirectory))
+            // {
+                // Directory.CreateDirectory(assetBundleDirectory);
+            // }
 
-            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, target);
+            // BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, target);
 
-            Debug.Log("AssetBundles built for platform: " + target.ToString());
-        }
+            // Debug.Log("AssetBundles built for platform: " + target.ToString());
+        // }
     }
 }
